694064e8c552ca18a583b140c94dc966f2b2b92b
avalor <avalormaquedano@gmail.com>
2012-03-21 12:56:39 +0100

    README

--- ejason.erl
+++ ejason.erl
@@ -1,3 +1,35 @@
+
+%% Copyright (c) 2012, Álvaro Fernández Díaz
+%% All rights reserved.
+%%
+%% Redistribution and use in source and binary forms, with or without
+%% modification, are permitted provided that the following conditions are met:
+%%     %% Redistributions of source code must retain the above copyright
+%%       notice, this list of conditions and the following disclaimer.
+%%     %% Redistributions in binary form must reproduce the above copyright
+%%       notice, this list of conditions and the following disclaimer in the
+%%       documentation and/or other materials provided with the distribution.
+%%     %% Neither the name of the copyright holders nor the
+%%       names of its contributors may be used to endorse or promote products
+%%       derived from this software without specific prior written permission.
+%%
+%% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ''AS IS''
+%% AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+%% IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+%% ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+%% BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+%% CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+%% SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR 
+%% BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+%% WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR 
+%% OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
+%% ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+%% @author Álvaro Fernández Díaz
+%% @copyright 2012 Álvaro Fernández Díaz
+%% @doc Represents the API to eJason, providing means to translate and run
+%%      eJason agents.
+
 -module(ejason).
 
 -export([run/1, run/2, run_test/1, run_test/2, parse/1,kill/1, kill/2,
@@ -15,6 +47,8 @@ run([Agent|List]) when is_tuple(Agent)->
 run([Agent|List]) when is_atom(Agent)->
     run(Agent,1),
     run(List).
+
+
 run(_Name,Num) when Num < 1 ->
     ok;
 run(Name,Num) ->
@@ -120,15 +154,4 @@ kill_test(Agent,Num)->
 	    ok
     end.
 
-
-
-
-%% test()->
-%%     parseAgents([agent2]).
-
-%% t()-> 
-%%     parseAgents([agent3]),
-%%     c:c(agent3).
-%% r()->
-%%     spawn(agent3,start,[]).
 
