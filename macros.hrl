641d9468e98df8e7ab226767552e7371a0085c69
avalor <avalormaquedano@gmail.com>
2012-04-12 17:04:03 +0200

    Macros

--- macros.hrl
+++ macros.hrl
@@ -7,4 +7,15 @@
 			 intentionSelector = fun reasoningCycle:selectIntention/1}).
 
 -record(event, {type = external, body = {}, relatedIntention=undefined}).
--record(plan,{trigger=fun(_X)-> false end,context, body}). %A function each
+-record(plan, {
+     trigger = fun(_X)-> false end, %% Function to match a plan trigger
+     context = fun(_X)->[] end,%% Function to match a plan context
+     body = [] }).%% A list of {function,bindings} pairs. Each function executes
+	       %% a plan body formula, the bindings state the positions in the
+               %% variables list for the plan that are used by the function. 
+
+-record(siPlan, %% Semi Instantiated Plan
+	{event = #event{},
+	plan = #plan{},
+	valuation = {},
+	formulas = []}).
